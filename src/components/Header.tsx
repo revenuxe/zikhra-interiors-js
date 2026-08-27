5e6991cf13a48293a743fe9e56f483d3a1475c62
revenuxe <revenuxe@gmail.com>
2026-08-27 23:12:34 +0530

    Align header with hero content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import logo from "@/assets/zikhra-design-logo.webp";
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 glass-effect">
-      <div className="flex items-center justify-start py-3 px-5">
+      <div className="mx-auto flex max-w-7xl items-center justify-start px-6 py-3 sm:px-10 lg:px-16">
         <Link href="/">
           <img src={logo.src} alt="Zikhra - Best Interior Designers in Bangalore" className="h-14 w-auto sm:h-16" />
         </Link>
